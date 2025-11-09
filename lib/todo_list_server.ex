1479be86bbe1988a42ae24fa6f5b2c068843e425
Loorinho <romeojacobson@gmail.com>
2025-11-09 18:41:03 +0300

    Converted my todo-list server into a GenServer

--- lib/todo_list_server.ex
+++ lib/todo_list_server.ex
@@ -1,41 +1,70 @@
 defmodule TodoListServer do
+  use GenServer
+
+  @impl GenServer
+  def init(initial_state) do
+    # initial_state is the is bound to the second argument passed into the GenServer.start function
+    {:ok, initial_state}
+  end
+
   def start do
     # starts a new process passin in an empty TodoList struct as the initial state
-    spawn(fn -> loop(TodoList.new()) end)
+    # spawn(fn -> loop(TodoList.new()) end)
+
+    GenServer.start(TodoListServer, TodoList.new(), name: __MODULE__)
   end
 
-  defp loop(initial_state) do
-    new_todo_list =
-      receive do
-        # delegates the message to the multiclause function where it is handled
-        message -> process_message(initial_state, message)
-      end
+  # defp loop(initial_state) do
+  #   new_todo_list =
+  #     receive do
+  #       # delegates the message to the multiclause function where it is handled
+  #       message -> process_message(initial_state, message)
+  #     end
 
-    loop(new_todo_list)
+  #   loop(new_todo_list)
+  # end
+
+  # def add_entry(todo_server, new_entry) do
+  #   send(todo_server, {:add_entry, new_entry})
+  # end
+
+  def add_entry(new_entry) do
+    GenServer.cast(__MODULE__, {:add_entry, new_entry})
   end
 
-  def add_entry(todo_server, new_entry) do
-    send(todo_server, {:add_entry, new_entry})
+  @impl GenServer
+  def handle_cast({:add_entry, new_entry}, state) do
+    {:noreply, TodoList.add_entry(state, new_entry)}
   end
 
-  def entries(todo_server, date) do
-    send(todo_server, {:entries, self(), date})
+  @impl GenServer
 
-    receive do
-      {:todo_entries, entries} -> entries
-    after
-      5000 -> {:error, :timeout}
-    end
+  def handle_call({:entries, date}, _from, state) do
+    {:reply, TodoList.entries(state, date), state}
   end
 
-  defp process_message(todo_list, {:add_entry, new_entry}) do
-    TodoList.add_entry(todo_list, new_entry)
+  def entries(date) do
+    GenServer.call(__MODULE__, {:entries, date})
   end
 
-  defp process_message(todo_list, {:entries, caller, date}) do
-    send(caller, {:todo_entries, TodoList.entries(todo_list, date)})
-    todo_list
-  end
+  # def entries(todo_server, date) do
+  #   send(todo_server, {:entries, self(), date})
+
+  #   receive do
+  #     {:todo_entries, entries} -> entries
+  #   after
+  #     5000 -> {:error, :timeout}
+  #   end
+  # end
+
+  # defp process_message(todo_list, {:add_entry, new_entry}) do
+  #   TodoList.add_entry(todo_list, new_entry)
+  # end
+
+  # defp process_message(todo_list, {:entries, caller, date}) do
+  #   send(caller, {:todo_entries, TodoList.entries(todo_list, date)})
+  #   todo_list
+  # end
 end
 
 defmodule TodoList do

78806ce4aaaf686a719c88877200d256d147a1e5
Loorinho <romeojacobson@gmail.com>
2025-12-13 21:44:44 +0300

    more elaborate supervisor which gives me more control incase i want to do extra initializations befor starting the children

--- lib/todo/system.ex
+++ lib/todo/system.ex
@@ -1,8 +1,19 @@
 defmodule Todo.System do
+  # def start_link do
+  #   Supervisor.start_link(
+  #     [Todo.Cache],
+  #     strategy: :one_for_one
+  #   )
+  # end
+
+  use Supervisor
+
+  # This approach gives more control more so if for example we want to do some extra initializations before starting the children. All this can be done in the init callback
   def start_link do
-    Supervisor.start_link(
-      [Todo.Cache],
-      strategy: :one_for_one
-    )
+    Supervisor.start_link(__MODULE__, nil)
+  end
+
+  def init(_) do
+    Supervisor.init([Todo.Cache], strategy: :one_for_one)
   end
 end
